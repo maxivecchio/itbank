d299b18917f5d66a2b5bfa6270a1a95e0d41d03e
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-08-26 11:33:24 -0300

    chore: Conversor

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -4,7 +4,7 @@ fetch('https://dolarapi.com/v1/dolares/oficial')
     .then(response => response.json())
     .then(data => {
         divCotizaciones.innerHTML = `
-        <div><span>Compra:</span><span class="cotizacion_numero">$${data.compra}</span></div>
-        <div><span>Venta:</span> <span class="cotizacion_numero">$${data.venta}</span></div>
+        <div><span>Compra</span><span class="cotizacion_numero">$${data.compra}</span></div>
+        <div><span>Venta</span> <span class="cotizacion_numero">$${data.venta}</span></div>
         `;
     })
