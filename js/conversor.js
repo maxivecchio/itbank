7feb09be99f8e63743afe11306a17a171dd21a30
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-08-26 12:06:46 -0300

    Validacion de input en el conversor

--- js/conversor.js
+++ js/conversor.js
@@ -1,14 +1,25 @@
 const form = document.querySelector('.conversor-form');
+const errorP = document.querySelector('#input-conversor-error');
+
+errorP.style.display = 'none';
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const cantidad = formData.get('ingresar-monto');
     const inputResultado = document.querySelector('#resultado');
-
     /* const valorDolar = 963;
     const resultado = cantidad / valorDolar;
     inputResultado.value = resultado; */
+
+    if (cantidad === '' || isNaN(cantidad) || cantidad <= 0) {
+        errorP.style.display = 'block';
+        inputResultado.value = '';
+        errorP.textContent = 'Por favor, ingrese un número válido';
+        return;
+    } else {
+        errorP.style.display = 'none';
+    }
 
     fetch(`https://dolarapi.com/v1/dolares/oficial`)
         .then(response => response.json())
